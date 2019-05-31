16e3a031336802135dac10bc68ae1732116886b4
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-31 11:53:28 -0600

    WIP i dont even know

--- README.md
+++ README.md
@@ -25,15 +25,19 @@ In this challenge, create a web page that presents a styled list of Star Wars ch
 Demonstrate your understanding of this Sprint's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
 
 - [ ] What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
+
 React is a library made by facebook to make it easier to pass around data and create components
 
 - [ ] What does it mean to _think_ in react?
+
 to understand what react is thinking
 
 - [ ] Describe state.
+
 state is the data our components hold on to an pass around to other components
 
 - [ ] Describe props.
+
 props are properties that can be passed to functional components as arguments
 
 ## Project Set Up
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,66 +3,67 @@ import React from 'react'
 function Character() {
     return(
         <div>
-            <h3>Name</h3>
+            <h3>{this.state.starwarsChars.name}</h3>
             <p>
                 <strong>Height</strong>   
+                {this.state.starwarsChars.height}
             </p>
             <p>
                 <strong>Mass</strong>
+                {this.state.starwarsChars.mass}
             </p>
-            
             <p>
                 <strong>Hair Color</strong>
+                {this.state.starwarsChars.hair_color}
             </p>
-            
             <p>
                 <strong>Skin Color</strong>
+                {this.state.starwarsChars.skin_color}
             </p>
-            
             <p>
                 <strong>Eye Color</strong>
+                {this.state.starwarsChars.eye_color}
             </p>
-            
             <p>
                 <strong>Birth Year</strong>
+                {this.state.starwarsChars.birth_year}
             </p>
-            
             <p>
                 <strong>Gender</strong>
+                {this.state.starwarsChars.gender}
             </p>
-            
             <p>
                 <strong>Homeworld</strong>
+                {this.state.starwarsChars.homeworld}
             </p>
-            
             <p>
                 <strong>Films</strong>
+                {this.state.starwarsChars.films}
             </p>
-            
             <p>
                 <strong>Species</strong>
+                {this.state.starwarsChars.species}
             </p>
-            
             <p>
                 <strong>Vehicles</strong>
+                {this.state.starwarsChars.vehicles}
             </p>
-            
             <p>
                 <strong>Starships</strong>
+                {this.state.starwarsChars.starships}
             </p>
-            
             <p>
                 <strong>Created</strong>
+                {this.state.starwarsChars.created}
             </p>
-            
             <p>
                 <strong>Edited</strong>
+                {this.state.starwarsChars.edited}
             </p>
-            
             <p>
                 <strong>Url</strong>
+                {this.state.starwarsChars.url}
             </p>
-            
         </div>
     )
 }
