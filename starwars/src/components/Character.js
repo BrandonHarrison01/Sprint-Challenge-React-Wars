8bd0914829abdfc250dbf73207f76dd83ca9760a
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-31 10:56:36 -0600

    WIP not looking good

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,70 @@
+import React from 'react'
+
+function Character() {
+    return(
+        <div>
+            <h3>Name</h3>
+            <p>
+                <strong>Height</strong>   
+            </p>
+            <p>
+                <strong>Mass</strong>
+            </p>
+            
+            <p>
+                <strong>Hair Color</strong>
+            </p>
+            
+            <p>
+                <strong>Skin Color</strong>
+            </p>
+            
+            <p>
+                <strong>Eye Color</strong>
+            </p>
+            
+            <p>
+                <strong>Birth Year</strong>
+            </p>
+            
+            <p>
+                <strong>Gender</strong>
+            </p>
+            
+            <p>
+                <strong>Homeworld</strong>
+            </p>
+            
+            <p>
+                <strong>Films</strong>
+            </p>
+            
+            <p>
+                <strong>Species</strong>
+            </p>
+            
+            <p>
+                <strong>Vehicles</strong>
+            </p>
+            
+            <p>
+                <strong>Starships</strong>
+            </p>
+            
+            <p>
+                <strong>Created</strong>
+            </p>
+            
+            <p>
+                <strong>Edited</strong>
+            </p>
+            
+            <p>
+                <strong>Url</strong>
+            </p>
+            
+        </div>
+    )
+}
+
+export default Character
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Character from './Character'
+
+function CharacterList () {
+    return(
+        <div>
+            <Character />
+
+            {/* {starwarsChars.map((character) => (
+                <Character character={character} />
+            ))} */}
+
+        </div>
+    )
+}
+
+export default CharacterList
