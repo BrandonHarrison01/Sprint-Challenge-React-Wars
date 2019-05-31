1fded7fac5ca28865711a27979fa544e01994282
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-05-31 15:45:13 -0600

    better styling on info cards

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,69 +1,40 @@
 import React from 'react'
 
+import './StarWars.css'
+
 function Character(props) {
     return(
-        <div style={{border: "1px solid black"}}>
+        <div className='cards'>
             <h3>{props.character.name}</h3>
             <p>
-                <strong>Height</strong>   
+                <strong>Height: </strong>   
                 {props.character.height}
             </p>
             <p>
-                <strong>Mass</strong>
+                <strong>Mass: </strong>
                 {props.character.mass}
             </p>
             <p>
-                <strong>Hair Color</strong>
+                <strong>Hair Color: </strong>
                 {props.character.hair_color}
             </p>
             <p>
-                <strong>Skin Color</strong>
+                <strong>Skin Color: </strong>
                 {props.character.skin_color}
             </p>
             <p>
-                <strong>Eye Color</strong>
+                <strong>Eye Color: </strong>
                 {props.character.eye_color}
             </p>
             <p>
-                <strong>Birth Year</strong>
+                <strong>Birth Year: </strong>
                 {props.character.birth_year}
             </p>
             <p>
-                <strong>Gender</strong>
+                <strong>Gender: </strong>
                 {props.character.gender}
             </p>
-            <p>
-                <strong>Homeworld</strong>
-                {props.character.homeworld}
-            </p>
-            <p>
-                <strong>Films</strong>
-                {props.character.films}
-            </p>
-            <p>
-                <strong>Species</strong>
-                {props.character.species}
-            </p>
-            <p>
-                <strong>Vehicles</strong>
-                {props.character.vehicles}
-            </p>
-            <p>
-                <strong>Starships</strong>
-                {props.character.starships}
-            </p>
-            <p>
-                <strong>Created</strong>
-                {props.character.created}
-            </p>
-            <p>
-                <strong>Edited</strong>
-                {props.character.edited}
-            </p>
-            <p>
-                <strong>Url</strong>
-                {props.character.url}
-            </p>
+
        </div>
     )
 }
