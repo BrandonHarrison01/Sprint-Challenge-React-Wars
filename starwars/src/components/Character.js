23bb16caeb440b243df309256f8fb117e5320e2d
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-07-12 10:29:05 -0600

    styling is complete

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Character(props) {
+
+    const Cards = styled.div`
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        margin-top: 40px;
+    `
+
+    const CharacterCard = styled.div`
+        border: 1px solid black;
+        background-color: rgba(32, 32, 32, 0.75);
+        width: 40%;
+        height: 250px;
+        margin-bottom: 40px;
+    `
+
     return(
-        <div>
+        <Cards>
             {props.characters.map(character => 
-                <div key={character.name}>
-                    <h3>{character.name}</h3>
-                    <h3>{character.height}</h3>
-                    <h3>{character.mass}</h3>
-                    <h3>{character.hair_color}</h3>
-                    <h3>{character.skin_color}</h3>
-                </div>  
+                <CharacterCard key={character.name}>
+                    <h2>Name: {character.name}</h2>
+                    <h3>Height: {character.height}</h3>
+                    <h3>Mass: {character.mass}</h3>
+                    <h3>Hair Color: {character.hair_color}</h3>
+                    <h3>Skin Color: {character.skin_color}</h3>
+                </CharacterCard>  
             )}
-        </div>
+        </Cards>
     )
 }
 
